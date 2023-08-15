19fac8a6d8e4c337b36d045a55e4322c7dc7bb2b
HicaroD <hdanrlley1@gmail.com>
2023-08-15 19:11:10 -0300

    feat(home): implementing home button

---
+++ js/home.js
@@ -0,0 +1,9 @@
+const learnMoreButtonPath = ".learn-more-button > button:nth-child(1)";
+
+const learnMoreButton = document.querySelector(learnMoreButtonPath);
+learnMoreButton.addEventListener("click", function () {
+  learnMoreButton.style.backgroundColor = "#7209b7";
+  setTimeout(function () {
+    learnMoreButton.style.backgroundColor = "#f72585";
+  }, 500);
+});
