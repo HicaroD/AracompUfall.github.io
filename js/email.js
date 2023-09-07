a397891470a5b846d5f5817425655a4cc6379343
HicaroD <hdanrlley1@gmail.com>
2023-09-07 14:04:48 -0300

    feat: using `mailto` for sending e-mail with form data

---
+++ js/email.js
@@ -0,0 +1,17 @@
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const to = "aaracompufal@gmail.com";
+  const name = document.getElementById("name-field").value;
+  const from = document.getElementById("email-field").value;
+  const subject = "CONTATO - ARACOMP";
+  const message = document.getElementById("message-field").value;
+
+  const emailBody = `mailto:${encodeURIComponent(
+    to
+  )}?cc=${name}&bcc=${from}&subject=${encodeURIComponent(
+    subject
+  )}&body=${encodeURIComponent(message)}`;
+
+  window.open(emailBody);
+});
